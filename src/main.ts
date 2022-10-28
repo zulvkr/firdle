3f1f864435f93450b4c5b749ae8cff81c6e03e34
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2022-10-29 00:22:36 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,16 @@ export const createApp = ViteSSG(
   { routes },
   ({ app, router, routes, isClient, initialState }) => {
     app.use(pinia)
+
+    if (import.meta.env.SSR) initialState.pinia = pinia.state.value
+    else pinia.state.value = initialState.pinia || {}
+
+    router.beforeEach((to, from, next) => {
+      // const store = useRootStore(pinia)
+      // if (!store.ready)
+      //   // perform the (user-implemented) store action to fill the store's state
+      //   store.initialize()
+      next()
+    })
   }
 )
