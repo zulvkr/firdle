3d83ca2bc0056bc4ad9133efdd95726b764f56c3
Ivan Zulfikar <ivan.zulf.ikar@gmail.com>
2022-11-16 19:20:05 +0700

    polish: use custom 100vh in other browser

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,12 @@ watchEffect(() => {
 @import url('./assets/fonts/fonts.css');
 :root {
   --100vh: 100vh;
-  /* Avoid Chrome to see Safari hack */
-  @supports (-webkit-touch-callout: none) {
+  --100vh: calc(var(--vh, 1vh) * 100);
+
+  /** use custom 100vh in iOS only */
+  /* @supports (-webkit-touch-callout: none) {
     --100vh: calc(var(--vh, 1vh) * 100);
-  }
+  } */
   --main-height: calc(var(--100vh) - var(--mini-app-bar-height));
   @screen sm {
     --main-height: calc(var(--100vh) - var(--app-bar-height));
